e437c829576fc0a9684116932ab01cc578f80a9e
Veloso-dev <velosoarts@gmail.com>
2022-05-16 20:20:06 -0300

    estudos

---
+++ modulos/calc.js
@@ -0,0 +1,6 @@
+var somafunc = require("./fun/somar");
+var divfunc = require("./fun/div");
+var multfunc = require("./fun/mult");
+var subfunc = require("./fun/sub");
+
+console.log(divfunc(2, 6));
