d9a058715b7710c6c59bc7ff2446d0ac96b875e7
Veloso-dev <velosoarts@gmail.com>
2022-05-17 18:52:58 -0300

    alterações

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express"); // serve para orientação de rotas
+const app = express();
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/html/index.html"); //toda vez que quero enviar algo para o servidor usso (send)
+});
+
+app.get("/sobre", function(req, res) {
+    res.sendFile(__dirname + "/html/sobre.html"); // caminho 
+})
+
+app.get("/blog", function(req, res) {
+    res.send("Bem-vindo ao meu blog");
+})
+
+app.get("/ola/:nome/:profissao", function(req, res) { // receber dados de uma função (req)
+    res.send("<h1> ola" + " " + req.params.nome + "<h1>"); //(.params. + o parametro ) exibe o parametro definido
+    // so pode enviar 1 sendo por bloco de codigo
+
+})
+
+
+
+app.listen(8081, function() {
+    console.log("Servidor rodando na url http://localhost:8081");
+});
